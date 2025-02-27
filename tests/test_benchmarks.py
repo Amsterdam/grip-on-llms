3d7e1fb0e0419284c083bb96d32d3a083afc3c57
Iva Gornishka <iva.gornishka@gmail.com>
2025-02-27 18:39:21 +0100

    added basic tests

---
+++ tests/test_benchmarks.py
@@ -0,0 +1,40 @@
+import os
+
+from llm_eval.benchmarks import MMLU_NL
+from llm_eval.language_models import LLMRouter
+
+
+def test_mmlu_nl():
+    gpt_params = {
+        "frequency_penalty": 0,
+        "presence_penalty": 0,
+        # "stop": None,
+    }
+
+    # Test GPT
+    gpt = LLMRouter.get_model(
+        provider="azure",
+        model_name="gpt-4o",
+        api_endpoint=os.environ["API_ENDPOINT"],
+        api_key=os.environ["API_KEY"],
+        api_version=os.environ["API_VERSION"],
+        params=gpt_params,
+    )
+
+    source = "http://nlp.uoregon.edu/download/okapi-eval/datasets/m_mmlu/nl_dev.json"
+    data_folder = "./data"
+    MMLU_NL_Bench = MMLU_NL(source, data_folder)
+
+    MMLU_NL_Bench.run(gpt)
+
+
+if __name__ == "__main__":
+
+
+    # test = "Test!"
+    test_prompt = "Hoe maak ik een melding in Amsterdam?"
+
+    # test_gpt(test_prompt)
+    # test_hf(test_prompt)
+
+    test_mmlu_nl()
---
+++ tests/test_llms.py
@@ -0,0 +1,55 @@
+import os
+
+from llm_eval.benchmarks import MMLU_NL
+from llm_eval.language_models import LLMRouter
+
+
+def test_gpt(test_prompt):
+    gpt_params = {
+        "frequency_penalty": 0,
+        "presence_penalty": 0,
+        # "stop": None,
+    }
+
+    # Test GPT
+    model = LLMRouter.get_model(
+        provider="azure",
+        model_name="gpt-4o",
+        api_endpoint=os.environ["API_ENDPOINT"],
+        api_key=os.environ["API_KEY"],
+        api_version=os.environ["API_VERSION"],
+        params=gpt_params,
+    )
+    print(f"GPT Response to {test}!: {model.prompt(test_prompt)}")
+    print(f"Second attempt {model(test_prompt)}")
+
+
+def test_hf(test_prompt):
+    hf_params = {
+        "do_sample": True,
+        "temperature": 0.6,
+        "top_p": 0.65,
+        # "top_k": 25,
+        "max_new_tokens": 200,
+        "no_repeat_ngram_size": 3,
+        "num_return_sequences": 1,
+    }
+
+    # Test HF Model
+    model = LLMRouter.get_model(
+        provider="huggingface",
+        model_name="tiny-llama1231",
+        hf_token=os.environ["HF_TOKEN"],
+#        hf_cache=os.environ["HF_CACHE"],
+        hf_cache=None,
+        params=hf_params,
+    )
+    print(f"HF Response to {test}!: {model.prompt(test_prompt)}")
+
+
+if __name__ == "__main__":
+    # test = "Test!"
+    test_prompt = "Hoe maak ik een melding in Amsterdam?"
+
+    test_gpt(test_prompt)
+    test_hf(test_prompt)
