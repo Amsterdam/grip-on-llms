b11dcbf33a9ee3a4f2cc507d11578a7c7f2f083c
Iva Gornishka <iva.gornishka@gmail.com>
2025-03-10 13:16:54 +0100

    test hf mmlu

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -5,7 +5,7 @@ from llm_eval.benchmarks import MMLU
 from llm_eval.language_models import LLMRouter
 
 
-def test_mmlu_nl():
+def test_mmlu_nl_gpt():
     gpt_params = {
         "frequency_penalty": 0,
         "presence_penalty": 0,
@@ -29,6 +29,34 @@ def test_mmlu_nl():
     mmlu_nl_bench.eval(gpt, "gpt_results")
 
 
+def test_mmlu_nl_hf():
+    hf_params = {
+        "do_sample": True,
+        "temperature": 0.6,
+        "top_p": 0.65,
+        # "top_k": 25,
+        "max_new_tokens": 200,
+        "no_repeat_ngram_size": 3,
+        "num_return_sequences": 1,
+    }
+
+    # Test HF Model
+    tinyllama = LLMRouter.get_model(
+        provider="huggingface",
+        model_name="tiny-llama",
+        hf_token=os.environ["HF_TOKEN"],
+        #        hf_cache=os.environ["HF_CACHE"],
+        hf_cache=None,
+        params=hf_params,
+    )
+
+    source = "http://nlp.uoregon.edu/download/okapi-eval/datasets/m_mmlu/nl_dev.json"
+    data_folder = "./data"
+    mmlu_nl_bench = MMLU(source, data_folder, categories=["moral_disputes"])
+
+    mmlu_nl_bench.eval(tinyllama, "tinyllama_results")
+
+
 if __name__ == "__main__":
     # test = "Test!"
     test_prompt = "Hoe maak ik een melding in Amsterdam?"
@@ -36,4 +64,5 @@ if __name__ == "__main__":
     # test_gpt(test_prompt)
     # test_hf(test_prompt)
 
-    test_mmlu_nl()
+    test_mmlu_nl_gpt()
+    test_mmlu_nl_hf()
