12eb49acf275f2ff49f2b4dbc047cff92fb70657
Iva Gornishka <i.gornishka@amsterdam.nl>
2025-03-20 13:56:04 +0000

    leaderboard implementation

---
+++ tests/test_leaderboard.py
@@ -0,0 +1,69 @@
+"""Basic tests to check whether a benchmark works as expected"""
+import logging
+
+from env_setup import get_gpt_secrets, get_hf_secrets
+
+from llm_eval.benchmarks import ARC, MMLU
+from llm_eval.language_models import LLMRouter
+from llm_eval.leaderboard import Leaderboard
+
+
+def test_leaderboard():
+    gpt_params = {
+        "frequency_penalty": 0,
+        "presence_penalty": 0,
+        # "stop": None,
+    }
+
+    logging.info("Initializing GPT")
+    gpt_secrets = get_gpt_secrets()
+    gpt = LLMRouter.get_model(
+        provider="azure",
+        model_name="gpt-4o",
+        api_endpoint=gpt_secrets["API_ENDPOINT"],
+        api_key=gpt_secrets["API_KEY"],
+        api_version=gpt_secrets["API_VERSION"],
+        params=gpt_params,
+    )
+
+    hf_secrets = get_hf_secrets()
+    hf_params = {
+        "do_sample": True,
+        "temperature": 0.6,
+        "top_p": 0.65,
+        # "top_k": 25,
+        "max_new_tokens": 200,
+        "no_repeat_ngram_size": 3,
+        "num_return_sequences": 1,
+    }
+
+    logging.info("Initializing some HF model")
+    tinyllama = LLMRouter.get_model(
+        provider="huggingface",
+        model_name="tiny-llama",
+        hf_token=hf_secrets["HF_TOKEN"],
+        #        hf_cache=os.environ["HF_CACHE"],
+        hf_cache=None,
+        params=hf_params,
+    )
+
+    logging.info("Setting up benchmarks")
+    mmlu_source = "http://nlp.uoregon.edu/download/okapi-eval/datasets/m_mmlu/nl_dev.json"
+    data_folder = "./data"
+    mmlu_nl_bench = MMLU("MMLU-NL", mmlu_source, data_folder, categories=["moral_disputes"])
+
+    arc_source = "http://nlp.uoregon.edu/download/okapi-eval/datasets/m_arc/nl_validation.json"
+    data_folder = "./data"
+    arc_nl_bench = ARC("ARC-NL", arc_source, data_folder, categories=["LEAP"])
+
+    logging.info("Running comparison")
+    leaderboard = Leaderboard(
+        llms=[gpt, tinyllama],
+        # llms = [tinyllama],
+        benchmarks=[mmlu_nl_bench, arc_nl_bench],
+    )
+    leaderboard.run_comparison(results_path="leaderboard")
+
+
+if __name__ == "__main__":
+    test_leaderboard()
