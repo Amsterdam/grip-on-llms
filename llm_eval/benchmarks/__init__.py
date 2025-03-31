86a9d93b768ee631cf1d5bf329ceda6b97720da3
Iva Gornishka <i.gornishka@amsterdam.nl>
2025-03-31 14:09:57 +0000

    added bert_score; fixed imports

--- llm_eval/benchmarks/__init__.py
+++ llm_eval/benchmarks/__init__.py
@@ -2,6 +2,6 @@
 
 from .arc import ARC
 from .mmlu import MMLU
-from .simplification import AmsterdamSimplification, INT_Duidelijke_Taal
+from .simplification import AmsterdamSimplification, INTDuidelijkeTaal
 
 __all__ = ["MMLU", "ARC", "INTDuidelijkeTaal", "AmsterdamSimplification"]
--- llm_eval/benchmarks/simplification/__init__.py
+++ llm_eval/benchmarks/simplification/__init__.py
@@ -1,6 +1,6 @@
 """Imports for simplification benchmarks."""
 
 from .amsterdam_simplification import AmsterdamSimplification
-from .int_duidelijke_taal import INT_Duidelijke_Taal
+from .int_duidelijke_taal import INTDuidelijkeTaal
 
 __all__ = ["INTDuidelijkeTaal", "AmsterdamSimplification"]
