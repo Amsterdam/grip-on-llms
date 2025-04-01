82a117516a874e155b8869664e4f4f466299d195
glo-ams <g.lo@amsterdam.nl>
2025-04-01 21:00:22 +0000

    Adds phi, falcon, llama and mistral models to existing set.

--- llm_eval/language_models/llms/llm_config.py
+++ llm_eval/language_models/llms/llm_config.py
@@ -38,4 +38,20 @@ MODEL_MAPPING = {
         "id": "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
         "kwargs": {},
     },
+    "mistral-7b-instruct-v0.3": {
+        "id": "mistralai/Mistral-7B-Instruct-v0.3",
+        "kwargs": {},
+    },
+    "llama-3.1-8b-instruct": {
+        "id": "meta-llama/Llama-3.1-8B-Instruct",
+        "kwargs": {},
+    },
+    "phi-4-mini-instruct": {
+        "id": "microsoft/Phi-4-mini-instruct",
+        "kwargs": {},
+    },
+    "falcon3-7b-instruct": {
+        "id": "tiiuae/Falcon3-7B-Instruct",
+        "kwargs": {},
+    },
 }
