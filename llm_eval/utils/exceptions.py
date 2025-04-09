cc479386bdcbe4e67ed11edfba3aa8214e8019f9
Iva Gornishka <i.gornishka@amsterdam.nl>
2025-04-09 14:30:17 +0000

    moved UnsupportedModelError to exceptions

--- llm_eval/utils/exceptions.py
+++ llm_eval/utils/exceptions.py
@@ -7,3 +7,20 @@ class EmptyResponseError(Exception):
     def __init__(self, message="The LLM returned an empty response."):
         self.message = message
         super().__init__(self.message)
+
+
+class UnsupportedModelError(Exception):
+    """Exception raised for unsupported models."""
+
+    def __init__(self, model_name=None, supported_models=None):
+        self.model_name = model_name
+        self.supported_models = supported_models
+        super().__init__(self._generate_message())
+
+    def _generate_message(self):
+        error_msg = (
+            f"Unsupported Model {self.model_name}" if self.model_name else "Unsupported Model.\n"
+        )
+        if self.supported_models:
+            error_msg += f"Choose from: {self.supported_models}"
+        return error_msg
