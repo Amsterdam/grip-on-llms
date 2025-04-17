c52c20da993e3c05c934fada21fddd222ef26690
Iva Gornishka <i.gornishka@amsterdam.nl>
2025-04-17 22:02:14 +0000

    add default prompt param to translators

--- llm_eval/translators/llm_based.py
+++ llm_eval/translators/llm_based.py
@@ -22,17 +22,29 @@ class LLMTranslator(BaseTranslator):
 
         self.llm = llm
 
-    def _translate(self, text, source_lang=None, target_lang=None, max_length=512):
+    def _translate(
+        self,
+        text,
+        source_lang=None,
+        target_lang=None,
+        max_length=512,
+        use_default_prompt=True,
+        **kwargs
+    ):
         """Translte text"""
         source_lang = source_lang if source_lang else self.source_lang
         target_lang = target_lang if target_lang else self.target_lang
 
-        prompt = default_prompt.format(
-            EXTRA_INSTRUCTIONS="",
-            SOURCE_LANG=source_lang,
-            TARGET_LANG=target_lang,
-            TEXT=text,
-        )
+        if use_default_prompt:
+            prompt = default_prompt.format(
+                EXTRA_INSTRUCTIONS="",
+                SOURCE_LANG=source_lang,
+                TARGET_LANG=target_lang,
+                TEXT=text,
+            )
+        else:
+            prompt = text
+
         translation = self.llm.prompt(prompt)
 
         return translation
