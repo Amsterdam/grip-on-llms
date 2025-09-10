a8dd48f01a74fcacb500f0676e1a81367452f530
Iva Gornishka <i.gornishka@amsterdam.nl>
2025-09-10 13:53:21 +0000

    rerun honesty with vllm

--- tests/test_leaderboard.py
+++ tests/test_leaderboard.py
@@ -360,27 +360,6 @@ def test_leaderboard():
             language="NL",
             data_dir=Path(benchmark_data_folder) / "BZK-Social-Bias-name",
         )
-
-    gpt_judge_params = {
-        "temperature": 0,
-        "top_p": 1,
-        # Don't penalize to ensure greedy decoding
-        "frequency_penalty": 0,
-        "presence_penalty": 0,
-        "n": 1,
-        # "stop": None,
-        "max_tokens": 20,
-    }
-
-    # Separate judge model to limig tokens and avoid accumulating codecarbon duration
-    gpt_4o_mini_judge = LLMRouter.get_model(
-        provider="azure",
-        model_name="gpt-4o-mini",
-        api_endpoint=gpt_secrets["API_ENDPOINT"],
-        api_key=gpt_secrets["API_KEY"],
-        api_version=gpt_secrets["API_VERSION"],
-        params=gpt_judge_params,
-        uses_api=True,
     )
 
     # Run HonestCity; skip judges, eval later
@@ -421,8 +400,8 @@ def test_leaderboard():
         + simple_benches
         + summary_benches
         + [mmlu_nl_bench]
-        + [arc_nl_bench],
-        + [mmlu_nl_bench + arc_nl_bench + honest_city_bench],
+        + [arc_nl_bench]
+        + [honest_city_bench],
         codecarbon_params=codecarbon_params,
         n_samples=n_samples,
     )
