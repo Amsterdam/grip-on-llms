ae7c6d95e3babb6a975fd46a5d0d714c7c31db45
Iva Gornishka <i.gornishka@amsterdam.nl>
2025-10-06 13:55:03 +0000

    handle openai requests failing due to content filters and limits

--- llm_eval/language_models/llms/openai.py
+++ llm_eval/language_models/llms/openai.py
@@ -1,5 +1,6 @@
 """Support for OpenAI models"""
 import logging
+import time
 from typing import List
 
 from openai import AzureOpenAI
@@ -31,57 +32,80 @@ class OpenAILLM(BaseLLM):
 
         return client
 
-    def _prompt(self, prompt, context=None, system=None, force_format=None):  # noqa
-        """Prompt model by optionally providing a custom system prompt or context"""
-        if not self.client:
-            self.client = self._get_client()
+    def _limit_requests(self, prompt):
+        limit_per_minute = 400000
+        limit_per_second = limit_per_minute / 60
+        time.sleep(len(prompt) / limit_per_second)
 
-        response = LLMResponse()
-        response.raw_prompt = prompt
-        try:
-            conversation = []
-            if system:
-                conversation.append({"role": "system", "content": system})
+    def _get_api_response(self, conversation, force_format):
+        if force_format:
+            if force_format == "json":
+                api_response = self.client.chat.completions.create(
+                    model=self.model_name,
+                    messages=conversation,
+                    **self.params,
+                    response_format={"type": "json_object"},
+                )
 
-            if context:
-                conversation.append({"role": "system", "content": context})
-
-            conversation.append({"role": "user", "content": prompt})
-            response.formatted_prompt = conversation
-
-            if force_format:
-                if force_format == "json":
-                    answer = self.client.chat.completions.create(
-                        model=self.model_name,
-                        messages=conversation,
-                        **self.params,
-                        response_format={"type": "json_object"},
-                    )
-
-                # Handled by base class
-                elif force_format == "multiple_choice":
-                    answer = self.client.chat.completions.create(
-                        model=self.model_name,
-                        messages=conversation,
-                        **self.params,
-                    )
-
-                else:
-                    raise NotImplementedError(
-                        "Currently there is no support for special formats other than json"
-                    )
-
-            else:
-                answer = self.client.chat.completions.create(
+            # Handled by base class
+            elif force_format == "multiple_choice":
+                api_response = self.client.chat.completions.create(
                     model=self.model_name,
                     messages=conversation,
                     **self.params,
                 )
 
-            finish_reason = answer.choices[0].finish_reason
+            else:
+                raise NotImplementedError(
+                    "Currently there is no support for special formats other than json"
+                )
+
+        else:
+            api_response = self.client.chat.completions.create(
+                model=self.model_name,
+                messages=conversation,
+                **self.params,
+            )
+
+        return api_response
+
+    def _get_formatted_conversation(self, prompt, context=None, system=None):
+        conversation = []
+        if system:
+            conversation.append({"role": "system", "content": system})
+
+        if context:
+            conversation.append({"role": "system", "content": context})
+
+        conversation.append({"role": "user", "content": prompt})
+        return conversation
+
+    def _prompt(self, prompt, context=None, system=None, force_format=None, limit_requests=False):
+        """Prompt model by optionally providing a custom system prompt or context"""
+        if not self.client:
+            self.client = self._get_client()
+
+        if limit_requests:
+            self._limit_requests(prompt)
+
+        response = LLMResponse()
+        response.raw_prompt = prompt
+        try:
+            conversation = self._get_formatted_conversation(prompt, context, system)
+            response.formatted_prompt = conversation
+
+            api_response = self._get_api_response(conversation, force_format)
+
+            finish_reason = api_response.choices[0].finish_reason
             if finish_reason != "stop":
                 logging.info(f"Finish reason: {finish_reason}")
-            response.raw_response = answer.choices[0].message.content
+
+            # Handle non-stop finish reasons which we want to treat as exceptions
+            if finish_reason == "content_filter":
+                response.error = True
+                response.exception = f"Request terminated with finish_reason: {finish_reason}"
+
+            response.raw_response = api_response.choices[0].message.content or ""
         except Exception as e:
             response.exception = str(e)
             response.error = True
